b30c6e3ff48accaa6efcd4a0f03b397f8d63bd2a
Erichain <jyamol520@gmail.com>
2017-07-03 22:49:13 +0800

    Summary: Fix config error

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: ['babel-loader?presets[]=react,presets[]=latest&compact=false'],
+        loader: ['babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-0'],
       }, {
         test: /\.scss$/,
         loader: [
